859255686d160c540ea5492597662d2e2e080e62
hsbcvietnam <hsbcvietnam.bank@gmail.com>
2022-12-14 12:15:23 +0700

    add functions

--- src/TopBar.js
+++ src/TopBar.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { styled, alpha, AppBar, Toolbar, IconButton, Typography, InputBase } from '@mui/material';
+import { styled, alpha, AppBar, Toolbar, Typography, InputBase, Backdrop, Modal, Box, Fade } from '@mui/material';
+import SignInBox from './SignInBox';
 
 const Search = styled('div')(({ theme }) => ({
     position: 'relative',
@@ -15,6 +16,20 @@ const Search = styled('div')(({ theme }) => ({
       width: 'auto',
     },
 }));
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '0',
+  borderRadius: '20px 20px 20px 20px',
+  boxShadow: 24,
+  p: 4,
+  outline: 0
+};
   
 const StyledInputBase = styled(InputBase)(({ theme }) => ({
     color: 'inherit',
@@ -33,33 +48,65 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
     },
 }));
 
-function TopBar() {
+function TopBar({isSignedIn, setIsSignedIn, open, handleOpen, handleClose}) {
   return (
     <AppBar position="static">
-        <Toolbar sx={{ background: '#000000' }}>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="open drawer"
-            sx={{ mr: 2 }}
-          >
-          </IconButton>
-          <Typography
-            variant="h6"
-            noWrap
-            component="div"
-            sx={{ flexGrow: 0, display: { xs: 'none', sm: 'block' }, marginRight: '20%' }}
-          >
-            Job Routing
-          </Typography>
-          <Search>
-            <StyledInputBase
-              placeholder="Search…"
-              inputProps={{ 'aria-label': 'search' }}
-            />
-          </Search>
-        </Toolbar>
+      <Toolbar sx={{ background: '#000000' }}>
+        <Typography
+          variant="h6"
+          noWrap
+          component="div"
+          sx={{ flexGrow: 0, display: { xs: 'none', sm: 'block' }, marginRight: '20%', marginLeft: '2.5%' }}
+        >
+          Job Routing
+        </Typography>
+        <Search>
+          <StyledInputBase
+            placeholder="Search…"
+            inputProps={{ 'aria-label': 'search' }}
+          />
+        </Search>
+        {
+          (isSignedIn) ? (
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' }, marginRight: '2.5%', textAlign: 'right' }}
+              onClick={() => setIsSignedIn(1-isSignedIn)}
+            >
+              Sign Out
+            </Typography>
+          ) : (
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' }, marginRight: '2.5%', textAlign: 'right' }}
+              onClick={handleOpen}
+            >
+              Sign In
+            </Typography>
+          )
+        }
+        <Modal
+          aria-labelledby="transition-modal-title"
+          aria-describedby="transition-modal-description"
+          open={open}
+          onClose={handleClose}
+          closeAfterTransition
+          BackdropComponent={Backdrop}
+          BackdropProps={{
+            timeout: 500,
+          }}
+        >
+          <Fade in={open}>
+            <Box sx={style}>
+              <SignInBox isSignedIn={isSignedIn} setIsSignedIn={setIsSignedIn} handleClose={handleClose} ></SignInBox>
+            </Box>
+          </Fade>
+        </Modal>
+      </Toolbar>
     </AppBar>
   )
 }
